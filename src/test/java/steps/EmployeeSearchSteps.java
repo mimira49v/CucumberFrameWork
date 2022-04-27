7cc5c9a14867f79bb5900223a277e7f490cd3f6a
mimie <mileusnic.milos97@gmail.com>
2022-04-26 21:28:14 -0400

    First Commit

--- src/test/java/steps/EmployeeSearchSteps.java
+++ src/test/java/steps/EmployeeSearchSteps.java
@@ -16,31 +16,42 @@ public class EmployeeSearchSteps extends CommonMethods {
     @When("user enters valid admin credentials")
     public void user_enters_valid_admin_credentials() {
         WebElement usernameField = driver.findElement(By.id("txtUsername"));
-        usernameField.sendKeys(ConfigReader.getPropertyValue("username"));
+//      usernameField.sendKeys(ConfigReader.getPropertyValue("username"));
+
+        sendText(usernameField, ConfigReader.getPropertyValue("username")); // instead, line 19
+//      passwordField.sendKeys(ConfigReader.getPropertyValue("password"));
+
         WebElement passwordField = driver.findElement(By.name("txtPassword"));
-        passwordField.sendKeys(ConfigReader.getPropertyValue("password"));
-        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+        sendText(passwordField, ConfigReader.getPropertyValue("password")); // instead line 25
+
     }
 
     @When("user clicks on login button")
     public void user_clicks_on_login_button() {
         WebElement loginBtn = driver.findElement(By.id("btnLogin"));
-        loginBtn.click();
-        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+        click(loginBtn);
+
+//      loginBtn.click();
+//      driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
     }
 
     @When("user navigated to employee list page")
     public void user_navigated_to_employee_list_page() {
         WebElement pimOption = driver.findElement(By.id("menu_pim_viewPimModule"));
-        pimOption.click();
+//      pimOption.click();
+
+        click(pimOption);
         WebElement empListOption = driver.findElement(By.id("menu_pim_viewPimModule"));
-        empListOption.click();
+//      empListOption.click();
+
+        click(empListOption);
     }
 
     @When("user enters valid employee id")
     public void user_enters_valid_employee_id() {
         WebElement searchId = driver.findElement(By.id("empsearch_id"));
-        searchId.sendKeys("8510142");
+//      searchId.sendKeys("8510142");
+        sendText(searchId, "8510142");
     }
 
     @When("user clicks on search button")
@@ -52,6 +63,7 @@ public class EmployeeSearchSteps extends CommonMethods {
     @Then("user is able to see employee information")
     public void user_is_able_to_see_employee_information() {
         System.out.println("Result displayed");
+        tearDown();
     }
 
     @Given("user is navigated to HRMS application")
@@ -61,7 +73,9 @@ public class EmployeeSearchSteps extends CommonMethods {
 
     @When("user enters valid employee name")
     public void user_enters_valid_employee_name() {
-      WebElement searchName = driver.findElement(By.id("empsearch_employee_name_empName"));
-      searchName.sendKeys("Zubair");
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+        WebElement searchName = driver.findElement(By.id("empsearch_employee_name_empName"));
+//      searchName.sendKeys("Zubair");
+        sendText(searchName, "Zubair");
     }
 }
--- src/test/resources/features/EmployeeSearch.feature
+++ src/test/resources/features/EmployeeSearch.feature
@@ -8,7 +8,7 @@ Feature: Search an employee in HRMS
     And user clicks on search button
     Then user is able to see employee information
 
-  Scenario: Search an employee by id
+  Scenario: Search an employee by name
     Given user is navigated to HRMS application
     When user enters valid admin credentials
     And user clicks on login button
