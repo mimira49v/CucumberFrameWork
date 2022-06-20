e1eb221c55e42053ad57a8c21af4522fa47ed4a0
mimie <mileusnic.milos97@gmail.com>
2022-06-19 22:49:33 -0400

    API automation

--- src/test/java/APISteps/GenerateTokenSteps.java
+++ src/test/java/APISteps/GenerateTokenSteps.java
@@ -5,11 +5,10 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import utils.APIConstants;
-
 import static io.restassured.RestAssured.given;
 
 public class GenerateTokenSteps {
-// Don't need it cuz we are passing APIConstants.GENERATE_TOKEN_URI on line 25.
+//  Don't need it cuz we are passing APIConstants.GENERATE_TOKEN_URI on line 25.
 //  String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
     public static String token;
 
