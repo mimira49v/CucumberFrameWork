a8ddbc1256b606c146f6259e407bcf8ee68a693e
mimira49v <mileusnic.milos97@gmail.com>
2024-11-16 21:13:23 -0500

    added a few methods in common methods

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,44 +1,36 @@
 package utils;
 
+import io.cucumber.java.sk.Tak;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;       // added this line 28 it's not a part of the original framework
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import steps.PageInitializers;
 import java.io.File;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import static java.awt.SystemColor.text;
 
 public class CommonMethods extends PageInitializers {
 
     public static WebDriver driver;
 
-    public void openBrowserAndLunchApplication() {
+    public void openBrowserAndLunchApplication(){
         ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
-        switch (ConfigReader.getPropertyValue("browser")) {
+        switch (ConfigReader.getPropertyValue("browser")){
             case "chrome":
-
-                ChromeOptions ChromeOptions = new ChromeOptions();
-                ChromeOptions.setHeadless(true); // prevents from opening chrome runs the tests without it
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver(ChromeOptions);
+                driver = new ChromeDriver();
                 break;
             case "firefox":
- /*
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
-        break;
-                */
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
             default:
                 throw new RuntimeException("Invalid browser name");
         }
@@ -48,111 +40,87 @@ public class CommonMethods extends PageInitializers {
         intializePageObjects();
     }
 
-    public static void sendText(WebElement element, String textToSend) {
+    public static void sendText(WebElement element, String textToSend){
         element.clear();
         element.sendKeys(textToSend);
     }
 
-    public static WebDriverWait getWait() {
+    public static WebDriverWait getWait(){
         WebDriverWait wait = new WebDriverWait(driver, Constants.EXPLICIT_WAIT);
         return wait;
     }
 
-    public static void Alert_handling(WebElement element) {
-        element = driver.findElement(By.id("alert"));
-        driver.switchTo().alert().dismiss();
-    }
-
-    public static void waitForClickability(WebElement element) {
+    public static void waitForClickability(WebElement element){
         getWait().until(ExpectedConditions.elementToBeClickable(element));
     }
 
-    public static void click(WebElement element) {
+    public static void click(WebElement element){
         waitForClickability(element);
         element.click();
     }
 
-    public static JavascriptExecutor getJSExecutor() {
+    public static JavascriptExecutor getJSExecutor(){
         JavascriptExecutor js = (JavascriptExecutor) driver;
         return js;
     }
 
-    public static void jsClick(WebElement element) {
+    public static void jsClick(WebElement element){
         getJSExecutor().executeScript("arguments[0].click;", element);
     }
 
-    public static byte[] takeScreenshot(String fileName) {
+    public static byte[] takeScreenshot(String fileName){
         TakesScreenshot ts = (TakesScreenshot) driver;
-        byte[] picBytes = ts.getScreenshotAs(OutputType.BYTES);
-        File sourceFile = ts.getScreenshotAs(OutputType.FILE);
-        try {
-            FileUtils.copyFile(sourceFile, new File(Constants.SCREENSHOT_FILEPATH + fileName + " " + getTimeStamp("yyyy-MM-dd-HH-mm-ss") + ".png"));
-        } catch (IOException e) {
-            e.printStackTrace();
+            byte[] picBytes = ts.getScreenshotAs(OutputType.BYTES);
+                File sourceFile = ts.getScreenshotAs(OutputType.FILE);
+                try {
+                    FileUtils.copyFile(sourceFile, new File(Constants.SCREENSHOT_FILEPATH + fileName+ " "+getTimeStamp("yyyy-MM-dd-HH-mm-ss")+".png"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return picBytes;
         }
-        return picBytes;
-    }
 
-    public static String getTimeStamp(String pattern) {
-        Date date = new Date();
+        public static String getTimeStamp(String pattern){
+            Date date = new Date();
 //          to format the date according to our choice we want to implement in this function
-        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-        return sdf.format(date);
-    }
-    public WebElement getElementBasedOnText (String text){
-        return driver.findElement(By.xpath("//[contains(text(), '"+text+"']"));
-    }
+            SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+            return sdf.format(date);
+        }
 
-    public static void tearDown() {
+    public static void tearDown(){
         driver.quit();
     }
 
-    public static void verifyErrorMsg(WebElement element) {
+    public static void verifyErrorMsg(WebElement element){
         System.out.println(element.isDisplayed());
     }
 
-    public static void getErrorMsg(WebElement element) {
+    public static void getErrorMsg(WebElement element){
         System.out.println(element.getText());
     }
+    public static void waitForVisibility(WebElement element) {
+        getWait().until(ExpectedConditions.visibilityOf(element));
+    }
 
-    public static void verifyLinkActive(String linkUrl) {
+    public static void waitForInvisibility(WebElement element) {
+        getWait().until(ExpectedConditions.invisibilityOf(element));
+    }
 
-        WebDriver driver = new ChromeDriver();
+    public static void waitForPresence(By locator) {
+        getWait().until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+    public static void selectByVisibleText(WebElement dropdown, String text) {
+        Select select = new Select(dropdown);
+        select.selectByVisibleText(text);
+    }
 
-        driver.manage().window().maximize();
+    public static void selectByIndex(WebElement dropdown, int index) {
+        Select select = new Select(dropdown);
+        select.selectByIndex(index);
+    }
 
-        driver.get(linkUrl);
-
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-
-        System.out.println("Total links are " + links.size());
-
-        for (int i = 0; i < links.size(); i++) {
-
-            WebElement ele = links.get(i);
-
-            String url = ele.getAttribute("href");
-
-            verifyLinkActive(url);
-
-        }
-        try {
-            URL url = new URL(linkUrl);
-
-            HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
-
-            httpURLConnect.setConnectTimeout(3000);
-
-            httpURLConnect.connect();
-
-            if (httpURLConnect.getResponseCode() == 200) {
-                System.out.println(linkUrl + " - " + httpURLConnect.getResponseMessage());
-            }
-            if (httpURLConnect.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND) {
-                System.out.println(linkUrl + " - " + httpURLConnect.getResponseMessage() + " - " + HttpURLConnection.HTTP_NOT_FOUND);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void selectByValue(WebElement dropdown, String value) {
+        Select select = new Select(dropdown);
+        select.selectByValue(value);
     }
 }
