2edd18490fdbc980aa9218a9f620a6b9d2c84ce1
Milos Mileusnic <mileusnic.milos97@gmail.com>
2025-04-01 13:19:11 -0400

    Cucumber reporting fixed for jenkins

--- src/test/java/runners/APIRunner.java
+++ src/test/java/runners/APIRunner.java
@@ -18,14 +18,7 @@ import org.junit.runner.RunWith;
 
         tags = "@dynamic",
 
-       // old plugin had to replace cuz of jenkins
-       // plugin = {"pretty", "html:target/report/cucumber.html", "json:target/report/cucumber.json", "junit:target/report/cucumber.xml"}
-       plugin = {
-                "pretty",
-                "html:target/cucumber/cucumber.html",       // or wherever you want
-                "json:target/cucumber/report.json",          // 🔥 change this path!
-                "junit:target/cucumber/cucumber.xml"
-        }
+        plugin = {"pretty", "html:target/report/cucumber.html", "json:target/report/cucumber.json", "junit:target/report/cucumber.xml"}
 )
 public class APIRunner {
 }
