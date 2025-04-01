187d44237427f814b01d1446180c2e0fb11431cd
Milos Mileusnic <mileusnic.milos97@gmail.com>
2025-04-01 13:17:06 -0400

    Latest code deltaAirlines

--- src/test/java/runners/APIRunner.java
+++ src/test/java/runners/APIRunner.java
@@ -18,7 +18,14 @@ import org.junit.runner.RunWith;
 
         tags = "@dynamic",
 
-        plugin = {"pretty", "html:target/report/cucumber.html", "json:target/report/cucumber.json", "junit:target/report/cucumber.xml"}
+       // old plugin had to replace cuz of jenkins
+       // plugin = {"pretty", "html:target/report/cucumber.html", "json:target/report/cucumber.json", "junit:target/report/cucumber.xml"}
+       plugin = {
+                "pretty",
+                "html:target/cucumber/cucumber.html",       // or wherever you want
+                "json:target/cucumber/report.json",          // 🔥 change this path!
+                "junit:target/cucumber/cucumber.xml"
+        }
 )
 public class APIRunner {
 }
